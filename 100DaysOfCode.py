3cd0f0fc69a4b61664381dff14156086eb5cadd2
imtheja <imtheja@gmail.com>
2021-04-24 01:03:57 -0400

    Day6

--- 100DaysOfCode.py
+++ 100DaysOfCode.py
@@ -102,7 +102,7 @@ if intUserChoice >=0 and intUserChoice < 3:
 
     print(listResult[intUserChoice][intComputersChoice])
 
-'''
+
 
 #Day5: Password Generator
 
@@ -134,3 +134,26 @@ random.shuffle(listPassword)
 strUPassword = ''.join(listPassword)
 print(f"Your password is: {strUPassword}")
 
+
+#Day6: Escaping the maze using while loops
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+        
+'''
